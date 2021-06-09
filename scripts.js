4113562ef1ba0c47cb6738cd8f6ed6f63d7f35bf
pdst101 <pdst101@gmail.com>
2021-06-09 20:01:32 +0100

    1st populate

--- scripts.js
+++ scripts.js
@@ -18,38 +18,27 @@ function makeRequest() {
         };
     });  
 }
-
 makeRequest();
 
 // Populate product card
-async function populateProductCards() {
-    const product = document.querySelector('.main');
-    const response = makeRequest();
-    const apiData = await response;
-    for (let i in apiData) {
-        const product = document.createElement('div');
-        const card = document.createElement('div');
-        const link = document.createElement('a');
-        const img = document.createElement('IMG');
-        const cardTitle = document.createElement('div');
-
-        product.classList.add('col-12', 'col-md-6');
-        card.classList.add('card');
-        img.classList.add('card-img-top');
-        cardTitle.classList.add('card-body', 'bg-main', 'text-center', 'text-light');
-
-        img.setAttribute('src', apiData[i].imageUrl);
-        link.setAttribute('href', 'product.html' + '?id=' + apiData[i]._id);
-
-
-        cardTitle.innerHTML = '<h5>' + apiData[i].name + ' - ' + apiData[i].price / 100 + "$" + '</h5>';
-
-        main.appendChild(product);
-        product.appendChild(link);
-        link.appendChild(card);
-        card.appendChild(img);
-        card.appendChild(cardTitle);
+async function populateProductCard() {
+    const productCard = document.querySelector('.main');
+    const apiRequest = makeRequest();
+    const dataArray = await apiRequest;
+    for (let i in dataArray) {
+    //Outer card div
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('col-12', 'col-md-6', 'col-lg-4', 'card');
+    productCard.appendChild(newDiv);
+    //Link to product page
+    const link = document.createElement('a');
+    link.setAttribute('href', 'product.html');
+    newDiv.appendChild(link);
+    //Product Picture
+    const picture = document.createElement('img');
+    picture.classList.add('card-img-top');
+    picture.setAttribute('src', dataArray[i].imageURL);
+    link.appendChild(picture);
     }
-
-}
-populateProducts();
+};
+populateProductCard();
