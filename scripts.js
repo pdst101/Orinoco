5b68c71b7a52d64d28e433e1729e27c2bcd39077
pdst101 <pdst101@gmail.com>
2021-06-22 16:02:21 +0100

    function for product page 1.0

--- scripts.js
+++ scripts.js
@@ -10,44 +10,87 @@ function makeRequest() {
             if (apiRequest.readyState === 4) {
                 if (apiRequest.status === 200) {
                     resolve(JSON.parse(apiRequest.response));
-                    console.log(JSON.parse(apiRequest.response))
+                    console.log(JSON.parse(apiRequest.response));
                 } else {
                     reject(JSON.parse(apiRequest.response));
                 }
             }
         };
-    });  
+    });
 }
-makeRequest();
 
 // Populate product card
 async function populateProductCards() {
     const productCard = document.getElementById('main');
-    const result = makeRequest();
-    const dataArray = await result;
+    const dataArray = await makeRequest();
     for (let i = 0; i < dataArray.length; i++) {
-    //Outer card div
-    const newDiv = document.createElement('div');
-    newDiv.classList.add('col-12', 'col-md-6', 'col-lg-4', 'card', 'card-block', 'd-flex');
-    productCard.appendChild(newDiv);
-    //Link to product page
-    const link = document.createElement('a');
-    link.setAttribute('href', 'product.html');
-    newDiv.appendChild(link);
-    //Product Picture
-    const picture = document.createElement('img');
-    picture.classList.add('card-img-top');
-    picture.setAttribute('src', dataArray[i].imageUrl);
-    link.appendChild(picture);
-    //Card body
-    const card = document.createElement('div');
-    card.classList.add('card-body', 'bg-main');
-    link.appendChild(card);
-    //Card title
-    const title = document.createElement('h4');
-    title.classList.add('card-title', 'text-center');
-    title.innerHTML = dataArray[i].name + " - " + dataArray[i].price/100 + '$';
-    card.appendChild(title);
+        //Outer card div
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('col-12', 'col-md-6', 'col-lg-4', 'card');
+        productCard.appendChild(newDiv);
+        //Link to product page
+        const link = document.createElement('a');
+        link.setAttribute('href', 'product.html?id=' + dataArray[i]._id);
+        newDiv.appendChild(link);
+        //Product picture
+        const picture = document.createElement('img');
+        picture.classList.add('card-img-top');
+        picture.setAttribute('src', dataArray[i].imageUrl);
+        link.appendChild(picture);
+        //Card body
+        const card = document.createElement('div');
+        card.classList.add('card-body', 'bg-main');
+        link.appendChild(card);
+        //Card title
+        const title = document.createElement('h4');
+        title.classList.add('card-title', 'text-center');
+        title.innerHTML = dataArray[i].name + " - " + dataArray[i].price / 100 + '$';
+        card.appendChild(title);
     };
 };
 populateProductCards();
+
+//Display on product page
+async function displayProduct() {
+    const dataArray = await makeRequest();
+    const link = window.location.href;
+    const currentId = link.split('id=')[1];
+    for (let i = 0; i < dataArray.length; i++) {
+        if (currentId === dataArray[i]._id) {
+            //Check
+            console.log(currentId)
+            console.log(dataArray[i]._id)
+            const productPage = document.getElementById('product-page');
+            //Product Picture
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('col-12', 'col-md-6');
+            productPage.appendChild(newDiv);
+            const picture = document.createElement('img');
+            picture.classList.add('w-100', 'w-md-50');
+            picture.setAttribute('src', dataArray[i].imageUrl);
+            newDiv.appendChild(picture);
+            //Product details
+            const details = document.createElement('div');
+            details.classList.add('col-12', 'col-md-6');
+            productPage.appendChild(details);
+            //Product title
+            const title = document.createElement('h4');
+            title.innerHTML = dataArray[i].name;
+            details.appendChild(title);
+            //Product price
+            const price = document.createElement('p');
+            price.innerHTML = dataArray[i].price / 100 + '$';
+            price.classList.add('text-success');
+            details.appendChild(price);
+            //Product description
+            const description = document.createElement('p');
+            description.innerHTML = dataArray[i].description;
+            details.appendChild(description);
+        };
+    };
+
+};
+
+displayProduct();
+
+
